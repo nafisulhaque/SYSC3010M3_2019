d05c5a727ebafcb25caeb98badb21acd1f925047
nafisulhaque <55394662+nafisulhaque@users.noreply.github.com>
2019-12-06 05:22:48 -0500

    Adds functionalities to Arduino and homeController
    - Adds duplex communication to Arduino
    - Adds json to homeController to send data to sever
    - Adds one way UDP communication to send json to homeController

--- homeController.py
+++ homeController.py
@@ -1,40 +1,80 @@
-import socket
-import sys
-
 import serial
-import time
+import globals
+import json
+from datetime import datetime
 
-#host = sys.argv[1]
-#textport = sys.argv[2]
+import socket, sys, time
 
-motion_act = 1
-#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#port = int(textport)
+host = sys.argv[1]
+textport = sys.argv[2]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = int(textport)
+server_address = (host, port)
+
 ser = serial.Serial('COM10', 9600)
 
-motion_detect = 1
+globals.initialize()
 
 
-def read_motion():
-    a = int(ser.readline().decode('UTF-8'))
-    global motion_detect
-    if a == 1:
-        motion_detect = 1
-    elif a == 0:
-        motion_detect = 0
+def parse_op_code(op_code_rec):
+    globals.pirActive = int(op_code_rec[0])
+    globals.lightsActive = int(op_code_rec[1])
+    globals.blindsActive = int(op_code_rec[2])
 
-def toggle_lights():
-    global motion_detect
-    if motion_detect == 1:
-        ser.write("Lights off".encode())
-        motion_detect = 0
-    elif motion_detect == 0:
-        ser.write("Lights on".encode())
+    globals.lightsOn = int(op_code_rec[3])
+    globals.blindsOn = int(op_code_rec[4])
+    globals.temp = int(op_code_rec[5:7])
+    globals.hum = int(op_code_rec[7:9])
+    globals.temp_hum_rec = int(op_code_rec[9:])
+
+
+def json_gen_send():
+    if globals.temp_hum_rec == 1:
+        temp_hum_dict = {
+            "commandtype": "insert",
+            "temp": globals.temp,
+            "hum": globals.hum,
+            "timeSent": str(datetime.now())
+        }
+
+    temp_hum_json = json.dumps(temp_hum_dict)
+
+    return temp_hum_json
+
+
+def comm_code_send():
+    ser.write("10110".encode('UTF-8'))
+
+
+def check_lights_active():
+    return globals.lightsActive
+
+
+def check_lights_on():
+    return globals.lightsOn
+
+
+def check_blinds_active():
+    return globals.blindsActive
+
+
+def check_blinds_on():
+    return globals.blindsOn
+
+
+def check_pir_active():
+    return globals.pirActive
+
+
+def check_hum_temp():
+    return globals.temp, globals.hum
+
 
 while True:
-    time.sleep(2)
-    toggle_lights()
-    read = ser.readline()
-    print(read.decode())
-    toggle_lights()
-
+    data = ser.readline().decode('UTF-8')
+    parse_op_code(data)
+    if globals.temp_hum_rec == 1:
+        json_gen = str(json_gen_send())
+        print(json_gen)
+        s.sendto(json_gen.encode('UTF-8'), server_address)
--- motion_lights/motion_lights.ino
+++ motion_lights/motion_lights.ino
@@ -1,65 +1,141 @@
 #include <Stepper.h>
+#include "DHT.h"
 
-#define LEDPIN 13             // choose the pin for the LED
+#define LEDPIN 12             // choose the pin for the LED
 #define PIRPIN 2              // choose the input pin (for PIR sensor)
-
+#define DHTPIN 7
 
 #define STEPS 200             //Number of steps
+#define DHTTYPE DHT11
 
 Stepper stepper(STEPS, 8, 10, 9, 11); //Setting up stepper
+DHT dht(DHTPIN, DHTTYPE);       //Setting up DHT
+
+int pirActive = HIGH;              // activates PIR motion sensor
+int blindsActive = HIGH;          // activates controls for blinds
+int lightsActive = HIGH;          // activates control for lights
 
 
-int pirState = LOW;             // we start, assuming no motion detected
-int val = 0;                    // variable for reading the pin status
-int blindsActive = 1;           // activates controls for blinds
-int lightsActive = 1;
-int timer = 0;
-String command;
+int pirState = LOW;               // we start, assuming no motion detected
+int val = LOW;                    // variable for reading the pin status
+
+int lightsOn = LOW;
+int blindsOn = LOW;
+
+int temp = 0;
+int hum = 0;
+
+int opCode;
+int commCode;
+
+unsigned long previousMillis  = 0;
+unsigned long currentMillis   = 0;
+unsigned long interval = 4000;
+
+int i = 0;
 void setup() {
   pinMode(LEDPIN, OUTPUT);      // declare LED as output
-  pinMode(PIRPIN, INPUT);     // declare sensor as input
+  pinMode(PIRPIN, INPUT);       // declare sensor as input
   stepper.setSpeed(60);         // set speed of stepper
- // dht.begin();
+  dht.begin();
+  currentMillis = millis();
   Serial.begin(9600);           // start serial
 }
- 
-void loop(){
-  val = digitalRead(PIRPIN);  // read input value
-  
+
+
+void loop() {
   if (Serial.available() > 0) {
-    command = Serial.readString();
-    }
-  if (command == "Lights off") {
-    lightsActive = 0;
-    Serial.println(command);
+    parse_command();
+    use_command();
   }
-  if (val == HIGH){
-    lightsOn();
+
+  if (pirActive == HIGH) {
+    use_PIR(digitalRead(PIRPIN));
+  }
+
+  if ((currentMillis - previousMillis) >= interval) {
+    humTempMonitor();
+    opCodeGen(1);
+    Serial.flush();
+    previousMillis = currentMillis;
   }
   else {
-    lightsOff();
+    opCodeGen(0);
+    Serial.flush();
+    currentMillis = millis();
   }
 }
 
 
-void lightsOn() {
-            // check if the input is HIGH
-    if (lightsActive == 1) {
-      digitalWrite(LEDPIN, HIGH); // turn LED ON
-    }
-    if (pirState == LOW && blindsActive == HIGH) {      
-      stepper.step(2048);       // turns the stepper once fully assuming it takes one turn to roll up blinds
-      pirState = HIGH;
-      Serial.println(pirState);   // for use by controller
-      
-    }
+void activateLB() {
+  if (lightsActive == HIGH) {
+    digitalWrite(LEDPIN, HIGH); // turn LED ON
+    lightsOn = HIGH;
   }
-  
-void lightsOff() {
-  digitalWrite(LEDPIN, LOW);  // turn LED OFF
-    if (pirState == HIGH && blindsActive == 1 && val == LOW){
-      stepper.step(-2048);    
-      pirState = LOW;
-      Serial.println(pirState);// for use by the controller
-    }
+
+  if (pirState == LOW && blindsActive == HIGH) {
+    stepper.step(2048);       // turns the stepper once fully assuming it takes one turn to roll up blinds
+    blindsOn = HIGH;
+    pirState = HIGH;
   }
+}
+
+void deactivateLB() {
+  if (lightsActive == HIGH) {
+    digitalWrite(LEDPIN, LOW);  // turn LED OFF
+    lightsOn = LOW;
+  }
+  if (pirState == HIGH && blindsActive == HIGH) {
+    stepper.step(-2048);
+    blindsOn = LOW;
+    pirState = LOW;
+  }
+}
+
+void use_PIR(int value) {
+  if (value == HIGH) {
+    activateLB();
+  }
+  else {
+    deactivateLB();
+  }
+}
+
+
+void humTempMonitor() {
+  temp = int(dht.readTemperature());
+  hum = int(dht.readHumidity());
+}
+
+void opCodeGen(int humTempRead) {
+  String tempS;
+  String humS;
+  if (String(hum).length() < 2) {
+    humS = "0" + String(hum);
+  }
+  else {
+    humS = String(hum);
+  }
+  if (String(temp).length() < 2) {
+    tempS = "0" + String(temp);
+  }
+  else {
+    tempS = String(temp);
+  }
+  String opCodeS = String(pirActive) + String(lightsActive) + String(blindsActive) + String(lightsOn) + String(blindsOn) + tempS + humS + String(humTempRead);
+  Serial.println(opCodeS);
+}
+
+
+
+void parse_command() {
+  commCode = Serial.readString().toInt();
+}
+
+void use_command() {
+  blindsOn = commCode % 10;
+  lightsOn = (commCode / 10) % 10;
+  blindsActive = (commCode / 100) % 10;
+  lightsActive = (commCode / 1000) % 10;
+  pirActive = (commCode / 10000) % 10;
+}
